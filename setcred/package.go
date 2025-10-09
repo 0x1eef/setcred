0a03445cd35a609a6e8362f0123933d1c27266ec
0x1eef <0x1eef@protonmail.com>
2025-10-09 19:12:03 +0000

    Mark constants as package-local

--- setcred/package.go
+++ setcred/package.go
@@ -9,21 +9,21 @@ import (
 
 type Option func(*setcred, *uint)
 
-const SYSCALL = int(591)
-const SETUID = uint(1) << 0
-const SETRUID = uint(1) << 1
-const SETSVUID = uint(1) << 2
-const SETGID = uint(1) << 3
-const SETRGID = uint(1) << 4
-const SETSVGID = uint(1) << 5
-const SETSUPPGROUPS = uint(1) << 6
-const SETMACLABEL = uint(1) << 7
+const sysnum = int(591)
+const setuid = uint(1) << 0
+const setruid = uint(1) << 1
+const setsvuid = uint(1) << 2
+const setgid = uint(1) << 3
+const setrgid = uint(1) << 4
+const setsvgid = uint(1) << 5
+const setsuppgroups = uint(1) << 6
+const setmaclabel = uint(1) << 7
 
 func SetCred(creds *setcred, flags uint) error {
-	flagsp := uintptr(flags)
-	credsp := uintptr(unsafe.Pointer(creds))
-	sizep := uintptr(unsafe.Sizeof(*creds))
-	_, _, err := syscall.Syscall6(uintptr(SYSCALL), flagsp, credsp, sizep, 0, 0, 0)
+	fptr := uintptr(flags)
+	cptr := uintptr(unsafe.Pointer(creds))
+	sptr := uintptr(unsafe.Sizeof(*creds))
+	_, _, err := syscall.Syscall6(uintptr(sysnum), fptr, cptr, sptr, 0, 0, 0)
 	if err != 0 {
 		return errors.New(fmt.Sprintf("errno: %d", err))
 	}
@@ -32,49 +32,49 @@ func SetCred(creds *setcred, flags uint) error {
 
 func SetUid(uid uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETUID
+		*flags |= setuid
 		creds.sc_uid = uid
 	}
 }
 
 func SetRuid(ruid uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETRUID
+		*flags |= setruid
 		creds.sc_ruid = ruid
 	}
 }
 
 func SetSvUid(svuid uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETSVUID
+		*flags |= setsvuid
 		creds.sc_svuid = svuid
 	}
 }
 
 func SetGid(gid uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETGID
+		*flags |= setgid
 		creds.sc_gid = gid
 	}
 }
 
 func SetRgid(rgid uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETRGID
+		*flags |= setrgid
 		creds.sc_rgid = rgid
 	}
 }
 
 func SetSvGid(svgid uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETSVGID
+		*flags |= setsvgid
 		creds.sc_svgid = svgid
 	}
 }
 
 func SetSuppGroups(groups ...uint32) Option {
 	return func(creds *setcred, flags *uint) {
-		*flags |= SETSUPPGROUPS
+		*flags |= setsuppgroups
 		creds.sc_supp_groups = uintptr(unsafe.Pointer(unsafe.SliceData(groups)))
 		creds.sc_supp_groups_nb = uint32(len(groups))
 	}
