2054ca97a6027f92328ca88c873cdfe27ba996bd
0x1eef <0x1eef@protonmail.com>
2025-10-09 19:03:39 +0000

    Move constants

--- setcred/package.go
+++ setcred/package.go
@@ -9,6 +9,16 @@ import (
 
 type Option func(*setcred, *uint)
 
+const SYSCALL = int(591)
+const SETUID = uint(1) << 0
+const SETRUID = uint(1) << 1
+const SETSVUID = uint(1) << 2
+const SETGID = uint(1) << 3
+const SETRGID = uint(1) << 4
+const SETSVGID = uint(1) << 5
+const SETSUPPGROUPS = uint(1) << 6
+const SETMACLABEL = uint(1) << 7
+
 func SetCred(creds *setcred, flags uint) error {
 	flagsp := uintptr(flags)
 	credsp := uintptr(unsafe.Pointer(creds))
